c7bd3a58a271a9a0f7c6e137301bb65eb8356888
pders01 <paulderscheid@gmail.com>
2023-02-12 22:03:55 +0100

    Add proper termination condition to partitionOverlappingIntervals

--- src/utils/partitionOverlappingIntervals.ts
+++ src/utils/partitionOverlappingIntervals.ts
@@ -3,7 +3,7 @@
  *  Example: https://stackoverflow.com/questions/30472556/how-to-find-all-overlapping-ranges-and-partition-them-into-chunks
  */
 
- export default function partitionOverlappingIntervals(intervals: Interval[]) {
+export default function partitionOverlappingIntervals(intervals: Interval[]) {
   const rightEndValues = intervals.map((r) => r.end).sort((a, b) => a - b);
   intervals.sort((a, b) => a.start - b.start);
 
@@ -14,18 +14,21 @@
   const groups = [];
   let cur = [];
 
-  // eslint-disable-next-line no-constant-condition
-  while (1) {
-    if (i < intervals.length && intervals[i].start < rightEndValues[j]) {
+  while (i < intervals.length && j < rightEndValues.length) {
+    if (intervals[i].start < rightEndValues[j]) {
       cur.push(intervals[i++]);
       ++active;
-    } else if (j < intervals.length) {
+    } else {
       ++j;
       if (--active === 0) {
         groups.push(cur);
         cur = [];
       }
-    } else break;
+    }
+  }
+
+  if (cur.length > 0) {
+    groups.push(cur);
   }
 
   return groups;
