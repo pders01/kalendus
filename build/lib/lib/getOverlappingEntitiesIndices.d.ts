741758e94cd22197d703f3415eb317e6b5202639
pders01 <paulderscheid@gmail.com>
2024-02-29 15:59:18 +0100

    chore: build new bundle

--- build/lib/lib/getOverlappingEntitiesIndices.d.ts
+++ build/lib/lib/getOverlappingEntitiesIndices.d.ts
@@ -1,2 +1,8 @@
-export default function getOverlappingEntitiesIndices(partitions: Interval[][]): Grading[];
+/**
+ * Calculates the indices and depths of overlapping entities within a set of partitions.
+ *
+ * @param {Array<Interval[]>} partitions - An array of partitions, where each partition is an array of intervals.
+ * @returns {Array<Grading>} An array of grading objects, representing indices and depths of overlapping entities.
+ */
+export default function getOverlappingEntitiesIndices(partitions: Array<Interval[]>): Array<Grading>;
 //# sourceMappingURL=getOverlappingEntitiesIndices.d.ts.map
