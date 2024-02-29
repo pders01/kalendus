5738b133025515161ed26524a4d1feb3c93bf927
pders01 <paulderscheid@gmail.com>
2024-02-29 15:37:09 +0100

    fix: filter out all-day entries before constructing the grading
    - This corrects the wrong offset that was previously produced by all-day entries still being evaluated for rendering in the grid.
    - Add a check for undefined values in the gradings at index and default to 100%.

--- src/lms-calendar.ts
+++ src/lms-calendar.ts
@@ -363,6 +363,14 @@ export default class LMSCalendar extends LitElement {
         if (!R.isEmpty(entriesByDate)) {
             grading = R.pipe(
                 entriesByDate,
+                R.filter(
+                    (entry) =>
+                        Number(entry.time?.end.hour) -
+                            Number(entry.time?.start.hour) <
+                            23 &&
+                        !entry.continuation.is &&
+                        !entry.continuation.has,
+                ),
                 R.map(({ time }) =>
                     this._getGridSlotByTime(time!)
                         .replace(/[^0-9/]+/g, '')
@@ -503,6 +511,10 @@ export default class LMSCalendar extends LitElement {
         grading: Grading[];
         index: number;
     }) {
+        if (!grading[index]) {
+            return 100;
+        }
+
         return (
             100 /
             grading.filter((item) => item.group === grading[index].group).length
