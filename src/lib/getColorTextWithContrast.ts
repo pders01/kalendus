3391136868018254ecd4384d782f7ea51907fac0
pders01 <paulderscheid@gmail.com>
2024-11-30 15:18:07 +0100

    refactor: add JSDoc for getColorTextWithContrast

--- src/lib/getColorTextWithContrast.ts
+++ src/lib/getColorTextWithContrast.ts
@@ -1,3 +1,9 @@
+/**
+ * Calculates the text color and background color based on the given color.
+ *
+ * @param {string} color - The color to process.
+ * @returns {[string, string]} An array containing the text color and background color.
+ */
 export default function getColorTextWithContrast(
     color?: string,
 ): [string, string] {
